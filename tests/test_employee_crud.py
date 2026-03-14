f1ceaa7fbd291b5c1cbc0f90ee2c8c022e93e126
Ashwarya206 <t.ashwarya@leadergroup.com>
2026-03-14 19:43:57 +0530

    test: add failing test for employee creation

---
+++ tests/test_employee_crud.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_create_employee():
+
+    response = client.post(
+        "/employees",
+        json={
+            "full_name": "John Doe",
+            "job_title": "QA Engineer",
+            "country": "India",
+            "salary": 100000
+        }
+    )
+
+    assert response.status_code == 200
+    assert response.json()["full_name"] == "John Doe"
